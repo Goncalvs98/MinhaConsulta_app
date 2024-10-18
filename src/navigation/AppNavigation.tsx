4252ae0799edae8f5b1caa35cd6affd10aa0555a
Goncalvs98 <goncgbr@gmail.com>
2024-10-18 19:39:55 -0300

    Criando conexões com consultations

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -20,40 +20,40 @@ export type RootStackParamList = {
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const AppNavigator = () => {
-  return (
-    <Stack.Navigator initialRouteName="Welcome">
-      <Stack.Screen 
-        name="Welcome" 
-        component={WelcomeScreen} 
-        options={{ title: 'Tela Inicial' }} // Título personalizável
-      />
-      <Stack.Screen 
-        name="Login" 
-        component={LoginScreen} 
-        options={{ title: 'Acesse sua conta' }} // Título personalizável
-      />
-      <Stack.Screen 
-        name="SignUp" 
-        component={SignUpScreen} 
-        options={{ title: 'Cadastrar' }} // Título personalizável
-      />
-      <Stack.Screen 
-        name="ConsultationsList" 
-        component={ConsultationsListScreen} 
-        options={{ title: 'Consultas Agendadas' }} // Título personalizável
-      />
-      <Stack.Screen 
-        name="ScheduleConsultation" 
-        component={ScheduleConsultationScreen} 
-        options={{ title: 'Agendar Consulta' }} // Título personalizável
-      />
-      <Stack.Screen 
-        name="ConfirmAppointment" 
-        component={ConfirmAppointmentScreen} 
-        options={{ title: 'Confirmação de Agendamento' }} // Título personalizável
-      />
-    </Stack.Navigator>
-  );
-};
-
-export default AppNavigator;
+    return (
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen 
+          name="Welcome" 
+          component={WelcomeScreen} 
+          options={{ title: 'Tela Inicial' }} 
+        />
+        <Stack.Screen 
+          name="Login" 
+          component={LoginScreen} 
+          options={{ title: 'Acesse sua conta' }} 
+        />
+        <Stack.Screen 
+          name="SignUp" 
+          component={SignUpScreen} 
+          options={{ title: 'Cadastrar' }} 
+        />
+        <Stack.Screen 
+          name="ConsultationsList" 
+          component={ConsultationsListScreen} 
+          options={{ title: 'Consultas Agendadas' }} 
+        />
+        <Stack.Screen 
+          name="ScheduleConsultation" 
+          component={ScheduleConsultationScreen} 
+          options={{ title: 'Agendar Consulta' }} 
+        />
+        <Stack.Screen 
+          name="ConfirmAppointment" 
+          component={ConfirmAppointmentScreen} 
+          options={{ title: 'Confirmação de Agendamento' }} 
+        />
+      </Stack.Navigator>
+    );
+  };
+  
+  export default AppNavigator;
